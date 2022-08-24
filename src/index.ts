27c82258633ec4466f89f13421d0fd943534dbc1
TA2LSM <semihsenol@gmail.com>
2022-08-24 15:48:14 +0300

    commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+// https://khalilstemmler.com/blogs/typescript/node-starter-project/
+
+// // Implicit Type
+// let firstName = "Semih";
+// firstName = 33; 
+// // Konsoldan "npx tsc" komutu ile derlemede hata verir.
+// // Derlemeden önce daha kodu yazarken de hata olarak gösterilecektir.
+// console.log(firstName);
+
+// Explict Type
+let lastName: string = "SENOL";
+lastName = 'TEST'
+console.log(lastName);
